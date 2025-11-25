04b05ba1c00c8b52f727a81ba86fc64eecc53643
Zahra Rassaf <98451722+Zahrarasaf@users.noreply.github.com>
2025-11-25 18:20:30 +0330

    Update statistical_tests.py

--- src/analysis/statistical_tests.py
+++ src/analysis/statistical_tests.py
@@ -1,32 +1,47 @@
 import pandas as pd
 import numpy as np
-from statsmodels.tsa.stattools import adfuller, grangercausalitytests
+from statsmodels.tsa.stattools import adfuller, kpss, grangercausalitytests
 from statsmodels.tsa.seasonal import seasonal_decompose
 import matplotlib.pyplot as plt
 
-class AdvancedStatisticalTests:
+class AdvancedStatisticalAnalyzer:
     def __init__(self, data):
         self.data = data
     
-    def stationarity_analysis(self, column):
-        """Comprehensive stationarity tests"""
-        result = adfuller(self.data[column].dropna())
+    def comprehensive_stationarity_test(self, column):
+        """ADF and KPSS tests for stationarity"""
+        # ADF Test
+        adf_result = adfuller(self.data[column].dropna())
+        
+        # KPSS Test  
+        kpss_result = kpss(self.data[column].dropna())
+        
         return {
-            'adf_statistic': result[0],
-            'p_value': result[1],
-            'is_stationary': result[1] < 0.05
+            'adf': {'statistic': adf_result[0], 'p_value': adf_result[1]},
+            'kpss': {'statistic': kpss_result[0], 'p_value': kpss_result[1]},
+            'is_stationary': adf_result[1] < 0.05 and kpss_result[1] > 0.05
         }
     
-    def granger_causality(self, target_country, cause_country, max_lag=7):
-        """Test if one country's data causes another"""
+    def seasonal_decomposition_analysis(self, column, period=365):
+        """Decompose time series into components"""
+        decomposition = seasonal_decompose(
+            self.data[column].dropna(), 
+            period=period, 
+            model='additive'
+        )
+        return decomposition
+    
+    def cross_country_causality(self, target_country, cause_country, max_lag=7):
+        """Granger causality between countries"""
         test_data = self.data[[target_country, cause_country]].dropna()
         result = grangercausalitytests(test_data, max_lag, verbose=False)
         
         p_values = [result[lag][0]['ssr_ftest'][1] for lag in range(1, max_lag+1)]
-        return min(p_values) < 0.05, p_values
-    
-    def seasonal_decomposition(self, column, period=365):
-        """Decompose time series into trend, seasonal, residual"""
-        decomposition = seasonal_decompose(self.data[column].dropna(), 
-                                         period=period, model='additive')
-        return decomposition
+        best_lag = np.argmin(p_values) + 1
+        best_p_value = min(p_values)
+        
+        return {
+            'causes': best_p_value < 0.05,
+            'best_lag': best_lag,
+            'p_value': best_p_value
+        }
