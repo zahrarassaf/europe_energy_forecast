c666917ddc02504230d8fd72bc148e14a1e14b79
Zahra Rassaf <98451722+Zahrarasaf@users.noreply.github.com>
2025-12-25 14:59:15 +0330

    Create init.py

---
+++ src/analysis/init.py
@@ -0,0 +1,13 @@
+"""
+Analysis package for Europe Energy Forecast.
+"""
+
+from .carbon_impact import CarbonImpactAnalyzer
+from .renewable_integration import RenewableIntegrationAnalyzer
+from .economic_analysis import EconomicAnalyzer
+
+__all__ = [
+    'CarbonImpactAnalyzer',
+    'RenewableIntegrationAnalyzer',
+    'EconomicAnalyzer'
+]
